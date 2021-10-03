5fd7f65c5057aa88634aa91c64d544f2d0dc5a2b
svr <svr@protonmail.com>
2021-10-03 15:49:57 +0300

    Добавлена кнопка

--- app/src/main/java/com/geekbrains/films/MainActivity.kt
+++ app/src/main/java/com/geekbrains/films/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.geekbrains.films
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener {}
     }
 }
