f27eac4d396a5282463faf4782efe8e85107da78
svr <svr@protonmail.com>
2021-10-03 16:21:01 +0300

    Показываем Film

--- app/src/main/java/com/geekbrains/films/MainActivity.kt
+++ app/src/main/java/com/geekbrains/films/MainActivity.kt
@@ -2,12 +2,16 @@ package com.geekbrains.films
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.button).setOnClickListener {}
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val f = Film("123def", "The Silence of the Lambs")
+            Toast.makeText(this, "f=$f", Toast.LENGTH_LONG).show()
+        }
     }
 }
