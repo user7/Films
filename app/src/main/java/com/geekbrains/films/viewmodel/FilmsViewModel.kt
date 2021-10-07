0c2810719eddb42d5aae33c5dde26e16f37712a9
svr <svr@protonmail.com>
2021-10-07 12:41:58 +0300

    cosmetics

--- app/src/main/java/com/geekbrains/films/viewmodel/FilmsViewModel.kt
+++ app/src/main/java/com/geekbrains/films/viewmodel/FilmsViewModel.kt
@@ -4,10 +4,10 @@ import androidx.lifecycle.ViewModel
 import com.geekbrains.films.model.Film
 
 class FilmsViewModel : ViewModel() {
-    private val data : ArrayList<Film> = arrayListOf(
+    private val data: ArrayList<Film> = arrayListOf(
         Film("1", "World War Z", "Zombies running all around."),
         Film("2", "28 Days Later", "Zombies eat British people!")
     )
 
-    fun getData() : List<Film> = data
+    fun getData(): List<Film> = data
 }
