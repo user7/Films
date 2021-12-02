fadc03e71c1576a24704b9a2f6f8677a0516ba8a
svr <svr@protonmail.com>
2021-12-02 05:10:44 +0300

    Поиск по адресу

--- app/src/main/java/com/geekbrains/films/view/map/MapsFragment.kt
+++ app/src/main/java/com/geekbrains/films/view/map/MapsFragment.kt
@@ -1,9 +1,8 @@
 package com.geekbrains.films.view.map
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.pm.PackageManager
 import android.graphics.Color
+import android.location.Address
+import android.location.Geocoder
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
@@ -11,7 +10,6 @@ import android.view.LayoutInflater
 import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
 import com.geekbrains.films.R
 import com.geekbrains.films.databinding.MapsFragmentBinding
 
@@ -24,7 +22,11 @@ import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PolylineOptions
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import java.io.IOError
+import java.io.IOException
 
 class MapsFragment : Fragment(), CoroutineScope by MainScope() {
     private lateinit var map: GoogleMap
@@ -34,7 +36,6 @@ class MapsFragment : Fragment(), CoroutineScope by MainScope() {
     private val binding get() = _binding!!
     private var i = 0;
 
-    @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
         map = googleMap
         map.uiSettings.isZoomControlsEnabled = true
@@ -60,6 +61,38 @@ class MapsFragment : Fragment(), CoroutineScope by MainScope() {
         )?.let { markers.add(it) }
     }
 
+    private fun initSearchByAddress() = with(binding) {
+        buttonAddressSearch.setOnClickListener {
+            val geoCoder = Geocoder(it.context)
+            val searchText = editAddressSearch.text.toString()
+            launch(Dispatchers.IO) {
+                try {
+                    val addresses = geoCoder.getFromLocationName(searchText, 1)
+                    if (addresses.isNotEmpty()) {
+                        goToAddress(addresses, it, searchText)
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    private fun goToAddress(addresses: List<Address>, it: View?, searchText: String) {
+        launch {
+            var location = LatLng(addresses[0].latitude, addresses[0].longitude)
+            setMarker(location, searchText)
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+        initSearchByAddress()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
@@ -72,11 +105,5 @@ class MapsFragment : Fragment(), CoroutineScope by MainScope() {
     ): View {
         _binding = MapsFragmentBinding.inflate(inflater, container, false)
         return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
     }
 }
