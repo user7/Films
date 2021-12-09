44c14b77d822609f906e650130d462afc114d737
svr <svr@protonmail.com>
2021-12-09 04:13:50 +0300

    Убрал ключ TMDB в local.properties, создал debug и release варианты.

--- app/src/main/java/com/geekbrains/films/model/repository/FilmRepository.kt
+++ app/src/main/java/com/geekbrains/films/model/repository/FilmRepository.kt
@@ -1,11 +1,12 @@
 package com.geekbrains.films.model.repository
 
 import android.graphics.Bitmap
+import com.geekbrains.films.BuildConfig
 import com.geekbrains.films.model.repository.transport.rest.FilmSearchResultDTO
 
 interface FilmRepository {
     companion object {
-        const val TMDB_KEY3 = "80741e82e9f539526693f267ad1f5b10"
+        val TMDB_KEY3: String = BuildConfig.TMDB_KEY3
     }
 
     fun findFilms(query: String, callback: (FilmSearchResultDTO) -> Unit)
